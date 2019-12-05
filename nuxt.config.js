5731e6ed2cd023c64af8d16c03a6d623ab63c2d1
Ethan Collins <ethantcollins98@gmail.com>
2019-12-05 15:17:16 -0700

    adds title, content, and icon to site

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,14 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Ethan Collins Portfolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: "I'm Ethan Collins, a Full-Stack Web Developer with a Computer Science background. Come check out my work!"}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
   /*
